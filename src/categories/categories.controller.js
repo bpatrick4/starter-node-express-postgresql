5bba97cf0804f62f8d213dc9e834a97c87db59f1
Brennan Ives <brennanives@gmail.com>
2024-06-07 21:40:07 -0400

    "Create starter-node-express-postgresql repository"

---
+++ src/categories/categories.controller.js
@@ -0,0 +1,11 @@
+const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
+const categoriesService = require("./categories.service");
+
+async function list(req, res) {
+  const data = await categoriesService.list();
+  res.json({ data });
+}
+
+module.exports = {
+  list: asyncErrorBoundary(list),
+};
